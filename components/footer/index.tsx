8834fce75a3e00c1336c3b085a79a4635b3ab708
xapski2671 <xapski2671@gmail.com>
2023-08-17 15:39:19 +0100

    ui start

---
+++ components/footer/index.tsx
@@ -0,0 +1,41 @@
+"use client"
+import Image from "next/image"
+import s from "./footer.module.sass"
+
+export default function Footer() {
+	return (
+		<footer className={s.container}>
+			<div>{"@2023 xps. All rights reserved"}</div>
+			<div className={s.social}>
+				<Image
+					src="/1.png"
+					width={15}
+					height={15}
+					className={s.icon}
+					alt="xps-fb"
+				/>
+				<Image
+					src="/2.png"
+					width={15}
+					height={15}
+					className={s.icon}
+					alt="xps-ig"
+				/>
+				<Image
+					src="/3.png"
+					width={15}
+					height={15}
+					className={s.icon}
+					alt="xps"
+				/>
+				<Image
+					src="/4.png"
+					width={15}
+					height={15}
+					className={s.icon}
+					alt="xps"
+				/>
+			</div>
+		</footer>
+	)
+}
---
+++ components/navbar/index.tsx
@@ -0,0 +1,56 @@
+"use client"
+import Link from "next/link"
+import s from "./navbar.module.sass"
+
+const links = [
+	{
+		id: 1,
+		title: "Home",
+		url: "/",
+	},
+	{
+		id: 2,
+		title: "Portfolio",
+		url: "/portfolio",
+	},
+	{
+		id: 3,
+		title: "Blog",
+		url: "/blog",
+	},
+	{
+		id: 4,
+		title: "About",
+		url: "/about",
+	},
+	{
+		id: 5,
+		title: "Contact",
+		url: "/contact",
+	},
+	{
+		id: 6,
+		title: "Dashboard",
+		url: "/dashboard",
+	},
+]
+
+export default function Navbar() {
+	return (
+		<div className={s.container}>
+			<Link href={"/"} className={s.logo}>
+				{"Lamania"}
+			</Link>
+			<div className={s.links}>
+				{links.map((link) => {
+					return (
+						<Link key={link.id} href={link.url} className={s.link}>
+							{link.title}
+						</Link>
+					)
+				})}
+				<button className={s.logout}>{"Logout"}</button>
+			</div>
+		</div>
+	)
+}
